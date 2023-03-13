6dabe8e873199cb5994553b1ff5af00bbd4e847e
RuiwangW <127743840+RuiwangW@users.noreply.github.com>
2023-03-13 21:59:50 +0800

    Add files via upload

---
+++ test.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 13 21:52:05 2023
+
+@author: 22193
+"""
+
+a test hhhh
+huio = ["jih", "asf", "afds"]
+for i in range(len(huio)):
+    print(f"you csa daf {huio[i]}")
+
+
+
+huio[1]
